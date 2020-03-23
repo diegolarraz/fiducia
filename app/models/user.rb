970e9ca5f30ddd6ddceb150f0a3011b0cfb75e34
diegolarraz <diegolarraz1@gmail.com>
2020-03-23 17:03:01 +0000

    made references for enquiry self reference

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :clients, foreign_key: :client_id , class_name: "Enquiry"
+  has_many :contractors, through: :clients
+  has_many :contractors, foreign_key: :contractor_id, class_name: "Enquiry"
+  has_many :clients, through: :contractors
 end
