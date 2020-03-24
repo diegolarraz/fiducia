f6fdec759070444d634b7cb7f5a42557136ab44f
Rory O'Neill <rory_oneill_1989@hotmail.com>
2020-03-24 12:25:58 +0000

    Routes, validations and associations complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get '/profile', to: 'pages#profile'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index, :show] do
+    resources :reviews, only: [:index]
+    resources :enquiries, only: [:index]
+    resources :jobs, only: [:index]
+  end
+  resources :enquiries, except: [:index, :show]
+  resources :reviews, except: [:index, :show]
+  resources :jobs, except: [:index, :show]
 end
