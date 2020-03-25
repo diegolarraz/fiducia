6dd0472cdcf2a846769f7803d007d30b72a1c749
diegolarraz <diegolarraz1@gmail.com>
2020-03-25 11:23:00 +0000

    refactored

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,13 @@
-Profession.create!(name: "Fontaneria", cost: 20.00)
-Profession.create!(name: "Pintura", cost: 20.00)
-Profession.create!(name: "Carpinteria", cost: 20.00)
-Profession.create!(name: "Electrico", cost: 20.00)
-Profession.create!(name: "Construccion", cost: 20.00)
-Profession.create!(name: "Gas y Calefaccion", cost: 20.00)
-Profession.create!(name: "Techos", cost: 20.00)
-Profession.create!(name: "Yeso", cost: 20.00)
+professions = ["Fontaneria", "Pintura", "Carpinteria", "Electrico", "Construccion", "Gas y Calefaccion", "Techos", "Yeso"]
+
+professions.each do |profession|
+  Profession.create!(name: profession, cost: 20.0)
+end
+# Profession.create!(name: "Fontaneria", cost: 20.00)
+# Profession.create!(name: "Pintura", cost: 20.00)
+# Profession.create!(name: "Carpinteria", cost: 20.00)
+# Profession.create!(name: "Electrico", cost: 20.00)
+# Profession.create!(name: "Construccion", cost: 20.00)
+# Profession.create!(name: "Gas y Calefaccion", cost: 20.00)
+# Profession.create!(name: "Techos", cost: 20.00)
+# Profession.create!(name: "Yeso", cost: 20.00)
