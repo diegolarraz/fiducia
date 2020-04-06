1772b51881e7f403be168de0d6824b72a32ec980
Rory O'Neill <rory_oneill_1989@hotmail.com>
2020-04-06 12:26:36 +0100

    improved logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
   def index
-    @users = @users = User.joins(:professions).where(professions: { id: params[:profession]})
+    @users = User.joins(:professions).where(professions: { id: params[:profession]})
   end
 end
