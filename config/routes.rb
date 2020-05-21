a7dcef2ed1a525acdfdb70a689746ab01a63b02e
diegolarraz <diegolarraz1@gmail.com>
2020-05-21 16:19:25 +0100

    added the button to homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get '/profile', to: 'pages#profile'
+  get '/coming_soon', to: 'pages#coming_soon'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show, :edit, :update, :delete] do
     resources :reviews, only: [:index]
